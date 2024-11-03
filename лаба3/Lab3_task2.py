e5d7513d00f34029c77c19f547f9bcc9b73d8b10
Stocks098 <voenhomat@gmail.com>
2024-11-03 23:41:59 +0300

    Add files via upload

---
+++ лаба3/Lab3_task2.py
@@ -0,0 +1,20 @@
+# TODO Напишите функцию find_common_participants
+def find_common_participants(a, b, m=","):
+    out = []
+    a = a.split(m)
+    b = b.split(m)
+
+    for i in a:
+        for j in b:
+            if i == j:
+                out.append(i)
+
+    out.sort()
+    return(out)
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+# TODO Провеьте работу функции с разделителем отличным от запятой
+
+print(find_common_participants(participants_first_group, participants_second_group, "|"))
